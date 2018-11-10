a008227d65d870dd72c162c2d9b18f700f94add6
Enrico Miglino <balearicdynamics@gmail.com>
2018-11-10 14:31:07 +0100

    Implemented the motion featuures
    Enabled the hardware switch to set the maintenace mode.
    Implemented the mouse emulation motion commands
    Created the single character command processor

--- Arduino/MKR1000_Vision/MKR1000_Vision.ino
+++ Arduino/MKR1000_Vision/MKR1000_Vision.ino
@@ -1,71 +1,71 @@
 /**
- * @file MKR1000_Vision.ino
- * @brief Main application to control HID via WiFi
- * 
- * This application implement both keyboard and mouse to control
- * a computer from remote via WiFi. The prototype application, for
- * impaired mobility persons works in conjunction with the
- * piVision prototype using a Raspberry PI3 B+ and a Omron Vision
- * camera device for movements and gestures detection.
- * 
- * @section sponsoring Main Sponsors
- * Dekimo Experts <dekimo.com>
- * Element14 <element14.com>
- * 
- * @section note Note on the Project
- * This prototype aims to explore the possibilities and limits
- * offered by the integration of the Omron Vision component creating
- * a smart device badrf on the Raspberry PI linux embedded and the
- * MKR1000 IoT Arduino family of microprocessors to create OSHW, Open Source
- * low cost and easy to use devices to support seriousl impaired mobility
- * users.\n
- * One of the most important considerations about the usability of this device
- * is its non-impacting approach to the host computer: no drivers or special
- * applications or software should be installed. The only condition is the
- * availability of on-screenkeyboard; this is part of the standard accessibility
- * features available on almost all the OS: Windows, Linux, OSX.
- * 
- * @section colors Color Coding
- * The MKR1000 Vision is a simple device to be attacched to a computer USB 2.0+
- * port. It is recognized as an external Keyboard/Mouse device. A 8-LEDs NeoPixel
- * RGB strip generates constant feedback to the user during the system startup and
- * the normal use. \n
- * 
- * @subsection systart On System Startup
- * When the system starts organge wiping scroll means the device is connecting to
- * the WiFi. If conection does not succeed, it flashes 5 times purple then stop
- * showing a fixed red light.
- * 
- * After the WiFi connection succeds, the device creates a TCP/IP server accessible on
- * the local network through the port 80 and the display flshes 5 times green, the it is
- * ready to receive command from the guest. If for some reason it is not possible to
- * initialize the server the device flashes 5 times the red light then shows a fixed red
- * light and stops.
- * 
- * When the startup procedure completes the device shows a fixed blue light for 5 seconds
- * then it is ready to receive commands.
- * 
- * @note If the blue light is fixed, the device is in development mode for programming and
- * testing. The received comands from the WiFi guest are sent to the serial terminal and
- * the device does not interact with the computer host. This is to prevent the difficult to
- * test, program and upgrade the device firmware when it runs as a HID device.
- * 
- * @subsection Operating Conditions
- * 
- * @version 0.1
- * @author Enrico Miglino <enrico.miglino@dekimo.com>
- * @author Balearic Dynamics <balearicdynamics@gmail.com>
- * @date November 2018
- * @license CC 3.0 ND-NC-SA
- */
- 
+   @file MKR1000_Vision.ino
+   @brief Main application to control HID via WiFi
+
+   This application implement both keyboard and mouse to control
+   a computer from remote via WiFi. The prototype application, for
+   impaired mobility persons works in conjunction with the
+   piVision prototype using a Raspberry PI3 B+ and a Omron Vision
+   camera device for movements and gestures detection.
+
+   @section sponsoring Main Sponsors
+   Dekimo Experts <dekimo.com>
+   Element14 <element14.com>
+
+   @section note Note on the Project
+   This prototype aims to explore the possibilities and limits
+   offered by the integration of the Omron Vision component creating
+   a smart device badrf on the Raspberry PI linux embedded and the
+   MKR1000 IoT Arduino family of microprocessors to create OSHW, Open Source
+   low cost and easy to use devices to support seriousl impaired mobility
+   users.\n
+   One of the most important considerations about the usability of this device
+   is its non-impacting approach to the host computer: no drivers or special
+   applications or software should be installed. The only condition is the
+   availability of on-screenkeyboard; this is part of the standard accessibility
+   features available on almost all the OS: Windows, Linux, OSX.
+
+   @section colors Color Coding
+   The MKR1000 Vision is a simple device to be attacched to a computer USB 2.0+
+   port. It is recognized as an external Keyboard/Mouse device. A 8-LEDs NeoPixel
+   RGB strip generates constant feedback to the user during the system startup and
+   the normal use. \n
+
+   @subsection systart On System Startup
+   When the system starts organge wiping scroll means the device is connecting to
+   the WiFi. If conection does not succeed, it flashes 5 times purple then stop
+   showing a fixed red light.
+
+   After the WiFi connection succeds, the device creates a TCP/IP server accessible on
+   the local network through the port 80 and the display flshes 5 times green, the it is
+   ready to receive command from the guest. If for some reason it is not possible to
+   initialize the server the device flashes 5 times the red light then shows a fixed red
+   light and stops.
+
+   When the startup procedure completes the device shows a fixed blue light for 5 seconds
+   then it is ready to receive commands.
+
+   @note If the blue light is fixed, the device is in development mode for programming and
+   testing. The received comands from the WiFi guest are sent to the serial terminal and
+   the device does not interact with the computer host. This is to prevent the difficult to
+   test, program and upgrade the device firmware when it runs as a HID device.
+
+   @subsection Operating Conditions
+
+   @version 0.1
+   @author Enrico Miglino <enrico.miglino@dekimo.com>
+   @author Balearic Dynamics <balearicdynamics@gmail.com>
+   @date November 2018
+   @license CC 3.0 ND-NC-SA
+*/
+
 #include "globals.h"
 
-/**
+/** ======================================================
    Application initialization.
    The setup include the AP connection and server setting
    waiting for character sending through TCP/IP protocol
-*/
+  ====================================================== */
 void setup() {
   int retries; ///< retries counter
 
@@ -127,7 +127,7 @@ void setup() {
 #endif
       errorState = true;
       flashSignalSequence(PIXRED);
-    setColor(PIXRED);
+      setColor(PIXRED);
     } // Responder startup error
     else {
       flashSignalSequence(PIXGREEN);
@@ -138,19 +138,27 @@ void setup() {
       // keyboard emulation is not enabled. The switch
       // state permits to program and debug the application
       // When the application runs in production the debug flag
-      // should be undefined
-      if (digitalRead(CONTROL_PIN) == false) {
+      // it is worth to #undef it.
+      if (digitalRead(CONTROL_PIN) == LOW) {
         Keyboard.begin();
+        Mouse.begin();
+        // Display is set to standby condition
+        colorWipe(CYAN, STRIP_WIPE_DELAY);
+        setColor(CYAN);
+        delay(INITIALIZED_DELAY);
+        setColor(PIXOFF);
+        strip.show(); // Initialize all pixels to 'off'
+        // Startup condition
+        hidStatus = HID_IDLE;
       } // Production mode
       else {
         // Note that in this case the error condition is used
         // to disable the loop (control pint is enabled so the
         // development mode is set
         errorState = true;
+        setColor(PIXWHITE);
       } // Development mode
-      // Display is set to standby condition
-      colorWipe(CYAN, STRIP_WIPE_DELAY);
-      setColor(CYAN);
+
     } // Responder started correctly
   } // WiFi connection is ok
 } // Setup
@@ -162,47 +170,173 @@ void setup() {
 */
 void loop() {
   // Loop runs only if no error condition has been generated
-  // by setup()
-  if (!errorState) {
+  // by setup() or until the user does not switch the device
+  // to maintenance mode
+  while ( (!errorState) && (digitalRead(CONTROL_PIN) == LOW)) {
     int j;
 
-    // Call the update() function on the MDNS responder 
-    // every loop iteration to make sure it can detect 
+    showHIDStatus();    // Update the first pixel status
+    // Call the update() function on the MDNS responder
+    // every loop iteration to make sure it can detect
     // and respond to the requests.
     mdnsResponder.poll();
 
     // listen for incoming clients
     WiFiClient client = server.available();
     if (client) {
-      Serial.println("new client");
-      while (client.connected()) {
+      hidStatus = HID_IDLEON;
+      showHIDStatus();
+      if (client.connected()) {
         if (client.available()) {
-          char c = client.read();
-          Serial.write(c);
-        }
-      }
-    }
+          hidStatus = HID_RECEIVE;
+          showHIDStatus();
+          clientCommand = client.read();
+          processCommand();
+        } // Client available
+        else {
+          // Client is connected but no new commands are sent,
+          // process eventually only the motion commands
+          processMotion();
+        } // No new commands received
+      } // client connected
+    } // Available data
+
+    // Check if the development mode has been set via
+    // hardware switch. After setting to the maintenance
+    // mode the device should be reset as this condition
+    // is reset during the setup() only.
     if (digitalRead(CONTROL_PIN)) {
+      Keyboard.end();
+      Mouse.end();
+      errorState = true;
+      setColor(PIXWHITE);
+#ifdef _DEBUG
+      Serial << "Switched to maintenance. Power Off to reset" << endl;
+#endif
+    } // Enabled development mode
+  } // while running condition
+} // Loop
 
-      //Keyboard.print("Keyboard Emulation Works");
-      Keyboard.print('Starting a light loop...!@#0((*)&@adfqropivjio');
-      delay(500);
+// =====================================================
+//  External Function
+// =====================================================
 
-      for (j = 0; j < 22; j++) {
-        Keyboard.press(KEY_BACKSPACE);
-      }
-
-      Keyboard.press( KEY_RETURN);
-
-      Keyboard.releaseAll();
+/**
+ * Executes the curret command process only if it
+ * is for motion
+ */
+void processMotion() {
+    if( (clientCommand == CURSOR_LEFT) ||
+    (clientCommand == CURSOR_RIGHT) ||
+    (clientCommand == CURSOR_UP) ||
+    (clientCommand == CURSOR_DOWN) ){
+        processCommand();
     }
-  } // No error condition
 }
 
-// =====================================================
-//  NeoPixel Function
-// =====================================================
+/**
+   Process the queued command from the client
+*/
+void processCommand() {
 
+  switch (clientCommand) {
+    case CURSOR_LEFT:
+      hidStatus = HID_MOTION;
+      Mouse.move(0 - MOUSE_STEPS, 0, 0);
+      break;
+    case CURSOR_RIGHT:
+      hidStatus = HID_MOTION;
+      Mouse.move(MOUSE_STEPS, 0, 0);
+      break;
+    case CURSOR_UP:
+      hidStatus = HID_MOTION;
+      Mouse.move(0, MOUSE_STEPS, 0);
+      break;
+    case CURSOR_DOWN:
+      hidStatus = HID_MOTION;
+      Mouse.move(0, 0 - MOUSE_STEPS, 0);
+      break;
+    case CURSOR_PAUSE:
+      hidStatus = HID_IDLEON;
+      break;
+    case CLICK_LEFT:
+      hidStatus = HID_MOTION;
+      break;
+    case CLICK_MID:
+      hidStatus = HID_MOTION;
+      break;
+    case CLICK_RIGHT:
+      hidStatus = HID_MOTION;
+      break;
+    case PRESS_LEFT:
+      hidStatus = HID_PRESSLEFT;
+      break;
+    case PRESS_MID:
+      hidStatus = HID_PRESSMID;
+      break;
+    case PRESS_RIGHT:
+      hidStatus = HID_PRESSRIGHT;
+      break;
+    case BUTTON_RELEASE:
+      hidStatus = HID_IDLEON;
+      break;
+  } // Comand case switch
+  showHIDStatus();
+}
+
+/**
+   Show the current HID status setting the leftmost pixel
+   to the corresponding color
+*/
+void showHIDStatus() {
+
+  switch (hidStatus) {
+    case HID_IDLE:
+      setStatusPix(CYAN);
+      break;
+    case HID_IDLEON:
+      setStatusPix(PIXWHITE);
+      break;
+    case HID_RECEIVE:
+      setStatusPix(FIRE5);
+      break;
+    case HID_DELAY:
+      setStatusPix(FIRE1);
+      break;
+    case HID_MOTION:
+      setStatusPix(PIXWHITE);
+      break;
+    case HID_PRESSLEFT:
+      setStatusPix(FIRE6);
+      break;
+    case HID_PRESSMID:
+      setStatusPix(BLUE4);
+      break;
+    case HID_PRESSRIGHT:
+      setStatusPix(PURPLE4);
+      break;
+  }
+}
+
+/**
+   Set the (first) status pixel color leaving
+   the other pixels intact
+
+   @param c The color
+*/
+void setStatusPix(uint32_t c) {
+  strip.setPixelColor(HID_PIXEL, c);
+  strip.show();
+  // Slowdown the interaction few ms. Change this value
+  // to fine tune the interaction sensitivity
+  delay(HID_PAUSE);
+}
+
+/**
+   Emits a single color flashing sequence
+
+   @param c The color
+*/
 void flashSignalSequence(uint32_t c) {
   int j;
 
@@ -211,10 +345,22 @@ void flashSignalSequence(uint32_t c) {
   }
 }
 
+/**
+   Call the delayLoop() function with a specific color
+
+   @param c The color
+*/
 void delayLoop(uint32_t c) {
   delayLoop(c, CONNECTION_DELAY_LOOP);
 }
 
+/**
+   Executes a delayed On/Off wipe sequence on a single
+   color for the desired number of times
+
+   @param c The color
+   @param cycle The number of wipe cycles
+*/
 void delayLoop(uint32_t c, int cycles) {
   int j;
 
@@ -224,7 +370,12 @@ void delayLoop(uint32_t c, int cycles) {
   }
 }
 
-//! Fill the dots one after the other with a color
+/**
+   Executes a wipe sequence of the desired color
+
+   @param c The color
+   @param wait The wipe delay between colors
+*/
 void colorWipe(uint32_t c, uint8_t wait) {
   for (uint16_t i = 0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
@@ -233,8 +384,13 @@ void colorWipe(uint32_t c, uint8_t wait) {
   }
 }
 
-//! Fill the dots one after the other with a color
-//! Then off in the same sequence
+/**
+   Executes a bidirectional wipe sequence
+   of the desired color
+
+   @param c The color
+   @param wait The wipe delay between colors
+*/
 void colorWipeOnOff(uint32_t c, uint8_t wait) {
   // Pixels on
   for (uint16_t i = 0; i < strip.numPixels(); i++) {
@@ -250,6 +406,12 @@ void colorWipeOnOff(uint32_t c, uint8_t wait) {
   }
 }
 
+/**
+   Flash thedesired color
+
+   @param c The color
+   @param wait The flash duration
+*/
 void colorFlash(uint32_t c, uint8_t wait) {
   setColor(c);
   delay(wait);
@@ -257,6 +419,11 @@ void colorFlash(uint32_t c, uint8_t wait) {
   delay(wait);
 }
 
+/**
+   Fill the bar with the desired color
+
+   @param c The color
+*/
 void setColor(uint32_t c) {
   uint16_t i;
 
@@ -278,7 +445,6 @@ void rainbow(uint8_t wait) {
   }
 }
 
-// Slightly different, this makes the rainbow equally distributed throughout
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
 
@@ -306,6 +472,12 @@ uint32_t Wheel(byte WheelPos) {
   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
+/**
+   Print on the serial the full WiFi status
+
+   @warning This function has effect only if
+   _DEBUG is defined
+*/
 void printWifiStatus() {
 #ifdef _DEBUG
   // print the SSID of the network you're attached to:
--- Arduino/MKR1000_Vision/globals.h
+++ Arduino/MKR1000_Vision/globals.h
@@ -1,7 +1,7 @@
 /**
- * @file globals.h
- * @brief Global parameters and constants
- */
+   @file globals.h
+   @brief Global parameters and constants
+*/
 #include <SPI.h>
 #include <WiFi101.h>
 #include <WiFiMDNSResponder.h>
@@ -17,7 +17,7 @@
 //! #undef _DEBUG to avoid serial log messages.
 //! @warning If the keyboard emulation is enabled (CONTROL_PIN true)
 //! the serial aoutput should be disabled (#undef _DEBUG)
-#define _DEBUG
+#undef _DEBUG
 
 #define SERVER_PORT 80
 //! Set the network SSID parameter depending on
@@ -40,28 +40,67 @@
 #define CONNECTION_DELAY_LOOP 20    ///< Number of color wipes while trying to connect
 #define CONNECTION_DELAY 25         ///< LED wiping delay per step
 #define FLASH_ERROR_LOOP 5          ///< Max tries to initialize the responder before
-                                    ///< generating a stable error condition
+///< generating a stable error condition
 #define CONNECTION_LOOP 5           ///< Number of flashing sequences
 #define CONNECTION_LOOP_DELAY 100   ///< Delay between flashing sequences
 #define MAX_CONNECTION_RETRIES 5    ///< Max tries to connect the WiFi before generating
-                                    ///< a stable error condition
+///< a stable error condition
 #define STRIP_WIPE_DELAY 200        ///< Default LED strip wiping delay
+#define INITIALIZED_DELAY 1000      ///< Signal the initialization has been completed
 
 //! If the condition error is set to true for some reason
 //! during the initialization process, the system is blocked
 //! and never start.
 boolean errorState = false;
 
+//! Command character received from the server
+char clientCommand;
+//! Status of the emulator
+//! The HID status reflects the display visualization
+int hidStatus;
+//! Current mouse x position
+int mouseX;
+//! Current mouse y position
+int mouseY;
+
+#define MOUSE_STEPS 5  ///< Mouse steps every move request
+
+#define HID_PIXEL 0     ///< The HID status pixel index in the NeoPix array
+
+#define HID_IDLE 1      ///< The HID is waiting
+#define HID_RECEIVE 2   ///< The HID has received a command (blink)
+#define HID_DELAY 3     ///< The HID is showing the delay progress
+#define HID_MOTION 4    ///< The HID cursor is moving
+#define HID_IDLEON 5    ///< The HID is waiting and a client is connected
+#define HID_PRESSLEFT 6     ///< The HID button is pressed (stay pressed)
+#define HID_PRESSMID 7      ///< The HID button is pressed (stay pressed)
+#define HID_PRESSRIGHT 8    ///< The HID button is pressed (stay pressed)
+#define HID_PAUSE 25    ///< Pause ms between every command execution
+
+// Hid motion commands
+#define CURSOR_LEFT 'L'     ///< Continuous motion in one direction
+#define CURSOR_RIGHT 'R'    ///< Continuous motion in one direction
+#define CURSOR_UP 'U'       ///< Continuous motion in one direction
+#define CURSOR_DOWN 'D'     ///< Continuous motion in one direction
+#define CURSOR_PAUSE 'P'    ///< Cursor is not moving
+#define CLICK_LEFT 'l'      ///< Mouse left button click
+#define CLICK_MID 'm'       ///< Mouse middle button click
+#define CLICK_RIGHT 'r'     ///< Mouse right button click
+#define PRESS_LEFT 'k'      ///< Mouse left button press
+#define PRESS_MID 'e'       ///< Mouse middle button press
+#define PRESS_RIGHT 'n'     ///< Mouse right button press
+#define BUTTON_RELEASE 'Q'  ///< Mouse button press released
+
 //! Predefined server port
 WiFiServer server(80);
 
-//! Create a MDNS responder to listen and respond 
+//! Create a MDNS responder to listen and respond
 //! to MDNS name requests.
 WiFiMDNSResponder mdnsResponder;
 
 //! Network key Index number (needed only for WEP)
 //! Not used here
-// int keyIndex = 0;             
+// int keyIndex = 0;
 
 int connectStatus = WL_IDLE_STATUS;
 
@@ -74,6 +113,7 @@ IPAddress ip(192, 168, 0, 5);
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
 // Predefined colors
+#define PIXOFF strip.Color(0, 0, 0)
 #define PIXPINK strip.Color(255, 64, 64)
 #define PIXWHITE strip.Color(255, 255, 255)
 #define PIXRED strip.Color(255, 0, 0)
